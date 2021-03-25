43b0496b2eee3bc4e714d69a308072352d1ba1d5
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-25 23:18:02 +0530

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,27 @@
-
+function validate(){
+    var name = document.forms["edit-menu"]["item-name"].value;
+    var price = document.forms["edit-menu"]["rs"].value;
+    var date = document.forms["edit-menu"]["launch-date"].value;
+    var category = document.forms["edit-menu"]["category"].value;
+    if (name == "") {
+        alert("Title is required");
+        return false;
+    } else if ((name.length < 2) || (name.length > 100)){
+        alert("Title should have 2 to 100 characters");
+        return false;
+    } else if (price == ""){
+        alert("Box Office is required");
+        return false;
+    } else if (isNaN(price)) {
+        alert("Box Office has to be a number");
+        return false;
+    } else if (date == "") {
+        alert("Date of Launch is required");
+        return false;
+    } else if (category == "") {
+        alert("Select one genre");
+        return false;
+    } else {
+        return true;
+    }
+}
